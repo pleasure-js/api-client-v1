fbf3dfe4dc26a0e7ce63d19f0958be22bc124648
Martin Rafael Gonzalez <tin@devtin.io>
2019-10-27 12:35:45 -0400

    improve build

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,14 @@ const minify = require('rollup-plugin-babel-minify')
 const commonjs = require('rollup-plugin-commonjs')
 const nodeResolve = require('rollup-plugin-node-resolve')
 const json = require('rollup-plugin-json')
-const builtins = require('rollup-plugin-node-builtins')
-const replace = require('rollup-plugin-replace')
+const rollupBuiltins = require('rollup-plugin-node-builtins')
+const rollupReplace = require('rollup-plugin-replace')
 const merge = require('deepmerge')
 const { getConfig: getApiConfig, getPlugins: getApiPlugins } = require('pleasure-api')
 const { port, entitiesUri, prefix } = getApiConfig()
 const { pluginsConfig: { jwt: { authEndpoint, revokeEndpoint } } } = getApiPlugins()
+const fs = require('fs')
+const path = require('path')
 
 const banner = `/*!
  * ${ packageName } v${ version }
@@ -16,25 +18,43 @@ const banner = `/*!
  * Released under the MIT License.
  */`
 
-const getPlugins = ({ minified = false, bundle = false } = {}) => {
+const getPlugins = ({ minified = false, bundle = false, replace = {} } = {}) => {
   const plugs = [
-    replace({
+    rollupReplace(Object.assign({
       VERSION: JSON.stringify(version),
       DEF_API_PORT: JSON.stringify(port),
       DEF_API_PREFIX: JSON.stringify(prefix),
       DEF_API_AUTH_ENDPOINT: JSON.stringify(authEndpoint),
       DEF_API_REVOKE_ENDPOINT: JSON.stringify(revokeEndpoint),
       DEF_API_ENTITIES_URI: JSON.stringify(entitiesUri)
-    }),
+    }, replace)),
     json()
   ]
 
   if (bundle) {
+    const externals = []
+    // objectHash
+    externals.push(fs.readFileSync(require.resolve('object-hash/dist/object_hash.js')).toString())
+    // axios
+    externals.push(fs.readFileSync(require.resolve('axios/dist/axios.js')).toString())
+    // socket.io-client
+    externals.push(fs.readFileSync(require.resolve('socket.io-client/dist/socket.io.js')).toString())
+    // socket.io-client
+    // externals.push(fs.readFileSync(require.resolve('socket.io-client/dist/socket.io.js')).toString())
+
+    fs.writeFileSync(path.join(__dirname, './dist/pleasure-api-client-deps.js'), externals.join(`\n`))
+
+    // console.log({ prepend })
     plugs.push(
-      builtins(),
+      // rollupInsert.prepend(prepend),
+      rollupBuiltins({
+        crypto: true
+      }),
       nodeResolve({
         jsnext: true,
-        main: true
+        main: true,
+        browser: true,
+        only: ['lodash', 'qs', 'jwt-decode', 'deepmerge', 'url']
       }),
       commonjs()
     )
@@ -52,6 +72,19 @@ const getPlugins = ({ minified = false, bundle = false } = {}) => {
 
 const plugins = getPlugins()
 const name = 'PleasureApiClient'
+
+const envReplace = {
+  'process.env.API_ERROR': JSON.stringify(false),
+  'process.server': false,
+  'process.client': true,
+  'process.env.PLEASURE_CLIENT_APP_URL': null,
+  'process.env.PLEASURE_CLIENT_APP_SERVER_URL': null,
+  'process.env.PLEASURE_MODE': null,
+  'process.env.PLEASURE_CLIENT_API_URL': null,
+  'process.env.PLEASURE_CLIENT_ENTITIES_URI': null,
+  'process.env.PLEASURE_CLIENT_AUTH_ENDPOINT': null,
+  'process.env.PLEASURE_CLIENT_REVOKE_ENDPOINT': null
+}
 
 module.exports = [
   {
@@ -80,10 +113,14 @@ module.exports = [
         banner
       }
     ],
-    plugins: getPlugins()
+    plugins: getPlugins({
+      bundle: true,
+      replace: envReplace
+    })
   },
   {
     input: 'src/pleasure-api-client.js',
+    // external: ['crypto'],
     output: [
       {
         file: 'dist/pleasure-api-client.min.js',
@@ -92,6 +129,10 @@ module.exports = [
         banner
       },
     ],
-    plugins: getPlugins({ minified: true })
+    plugins: getPlugins({
+      bundle: true,
+      minified: true,
+      replace: envReplace
+    })
   },
 ]
